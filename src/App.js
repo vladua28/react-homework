10340c121af4681f47feb282ce182c0e2b7d716c
VLAD OSTAFIYCHUK <vladua28@gmail.com>
2023-07-04 19:54:15 +0300

    add hw1_2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Character from './components/Character/Character.jsx';
+import styles from './components/Character/Character.module.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className={styles.characterContainer}>
+            <Character
+                id={1}
+                name="Rick Sanchez"
+                status="Alive"
+                species="Human"
+                gender="Male"
+                src="https://rickandmortyapi.com/api/character/avatar/1.jpeg"
+            />
+            <Character
+                id={2}
+                name="Morty Smith"
+                status="Alive"
+                species="Human"
+                gender="Male"
+                src="https://rickandmortyapi.com/api/character/avatar/2.jpeg"
+            />
+            <Character
+                id={3}
+                name="Summer Smith"
+                status="Alive"
+                species="Human"
+                gender="Female"
+                src="https://rickandmortyapi.com/api/character/avatar/3.jpeg"
+            />
+            <Character
+                id={4}
+                name="Beth Smith"
+                status="Alive"
+                species="Human"
+                gender="Female"
+                src="https://rickandmortyapi.com/api/character/avatar/4.jpeg"
+            />
+            <Character
+                id={5}
+                name="Jerry Smith"
+                status="Alive"
+                species="Human"
+                gender="Male"
+                src="https://rickandmortyapi.com/api/character/avatar/5.jpeg"
+            />
+            <Character
+                id={6}
+                name="Summer Smith"
+                status="Alive"
+                species="Human"
+                gender="Female"
+                src="https://rickandmortyapi.com/api/character/avatar/6.jpeg"
+            />
+        </div>
+    );
 }
 
 export default App;
